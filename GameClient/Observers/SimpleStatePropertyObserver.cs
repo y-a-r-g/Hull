a362077be8d52e8d239e9d8c326f46ac49f20ebe
Sergey Dvoynikov <svdvoynikov@gmail.com>
2017-05-15 08:16:46 +0500

    SimpleStatePropertyObserver was added

---
+++ GameClient/Observers/SimpleStatePropertyObserver.cs
@@ -0,0 +1,28 @@
+﻿using Hull.GameClient.Interfaces;
+using Hull.GameServer.Interfaces;
+using Hull.GameServer.ServerState;
+
+namespace Hull.GameClient.Observers {
+    public class SimpleStatePropertyObserver<TProperty> : IStateObserver
+        where TProperty : IStateProperty {
+        public delegate void ObservedSimpleStatePropertyChangedDelegate(
+            TProperty property,
+            State state);
+
+        public event ObservedSimpleStatePropertyChangedDelegate ObservedSimpleStatePropertyChanged;
+
+        private readonly TProperty _property;
+
+        public SimpleStatePropertyObserver(TProperty property) {
+            _property = property;
+        }
+
+        public void OnStateChange(State state) {
+            if (ObservedSimpleStatePropertyChanged != null) {
+                if (_property.IsModified) {
+                    ObservedSimpleStatePropertyChanged(_property, state);
+                }
+            }
+        }
+    }
+}
